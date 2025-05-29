394f7a98a960b729464a69cff87f3a4175e14a3e
Артур <@DESKTOP-RGMQBHE>
2025-05-29 19:04:38 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace VanyaCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxPlayers = new ListBox();
+            buttonAddPlayer = new Button();
+            textBoxNewPlayer = new TextBox();
+            buttonReset = new Button();
+            buttonIncreaseGame = new Button();
+            buttonDeletePlayer = new Button();
+            buttonResetGames = new Button();
+            tabControl1 = new TabControl();
+            tabPageMain = new TabPage();
+            buttonDecreaseGames = new Button();
+            tabPageCurrentGame = new TabPage();
+            buttonRejectGame = new Button();
+            buttonAcceptGame = new Button();
+            listBoxCurrentGame = new ListBox();
+            buttonAddToCurrentGame = new Button();
+            comboBoxSearchPlayers = new ComboBox();
+            tabControl1.SuspendLayout();
+            tabPageMain.SuspendLayout();
+            tabPageCurrentGame.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBoxPlayers
+            // 
+            listBoxPlayers.FormattingEnabled = true;
+            listBoxPlayers.ItemHeight = 15;
+            listBoxPlayers.Location = new Point(5, 6);
+            listBoxPlayers.Name = "listBoxPlayers";
+            listBoxPlayers.Size = new Size(270, 304);
+            listBoxPlayers.TabIndex = 0;
+            // 
+            // buttonAddPlayer
+            // 
+            buttonAddPlayer.Location = new Point(281, 6);
+            buttonAddPlayer.Name = "buttonAddPlayer";
+            buttonAddPlayer.Size = new Size(205, 40);
+            buttonAddPlayer.TabIndex = 1;
+            buttonAddPlayer.Text = "Добавить игрока";
+            buttonAddPlayer.UseVisualStyleBackColor = true;
+            buttonAddPlayer.Click += buttonAddPlayer_Click;
+            // 
+            // textBoxNewPlayer
+            // 
+            textBoxNewPlayer.Location = new Point(281, 52);
+            textBoxNewPlayer.Name = "textBoxNewPlayer";
+            textBoxNewPlayer.Size = new Size(205, 23);
+            textBoxNewPlayer.TabIndex = 2;
+            // 
+            // buttonReset
+            // 
+            buttonReset.Location = new Point(3, 316);
+            buttonReset.Name = "buttonReset";
+            buttonReset.Size = new Size(270, 23);
+            buttonReset.TabIndex = 3;
+            buttonReset.Text = "Отсортировать список";
+            buttonReset.UseVisualStyleBackColor = true;
+            buttonReset.Click += buttonReset_Click;
+            // 
+            // buttonIncreaseGame
+            // 
+            buttonIncreaseGame.Location = new Point(281, 81);
+            buttonIncreaseGame.Name = "buttonIncreaseGame";
+            buttonIncreaseGame.Size = new Size(205, 39);
+            buttonIncreaseGame.TabIndex = 4;
+            buttonIncreaseGame.Text = "+1 счётчик игр";
+            buttonIncreaseGame.UseVisualStyleBackColor = true;
+            buttonIncreaseGame.Click += buttonIncreaseGame_Click;
+            // 
+            // buttonDeletePlayer
+            // 
+            buttonDeletePlayer.Location = new Point(281, 171);
+            buttonDeletePlayer.Name = "buttonDeletePlayer";
+            buttonDeletePlayer.Size = new Size(205, 39);
+            buttonDeletePlayer.TabIndex = 5;
+            buttonDeletePlayer.Text = "Удалить выбранного игрока";
+            buttonDeletePlayer.UseVisualStyleBackColor = true;
+            buttonDeletePlayer.Click += buttonDeletePlayer_Click;
+            // 
+            // buttonResetGames
+            // 
+            buttonResetGames.Location = new Point(3, 345);
+            buttonResetGames.Name = "buttonResetGames";
+            buttonResetGames.Size = new Size(270, 23);
+            buttonResetGames.TabIndex = 6;
+            buttonResetGames.Text = "Обнулить игры";
+            buttonResetGames.UseVisualStyleBackColor = true;
+            buttonResetGames.Click += buttonResetGames_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPageMain);
+            tabControl1.Controls.Add(tabPageCurrentGame);
+            tabControl1.Location = new Point(8, 9);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(500, 408);
+            tabControl1.TabIndex = 7;
+            // 
+            // tabPageMain
+            // 
+            tabPageMain.Controls.Add(buttonDecreaseGames);
+            tabPageMain.Controls.Add(buttonAddPlayer);
+            tabPageMain.Controls.Add(buttonResetGames);
+            tabPageMain.Controls.Add(listBoxPlayers);
+            tabPageMain.Controls.Add(buttonDeletePlayer);
+            tabPageMain.Controls.Add(textBoxNewPlayer);
+            tabPageMain.Controls.Add(buttonIncreaseGame);
+            tabPageMain.Controls.Add(buttonReset);
+            tabPageMain.Location = new Point(4, 24);
+            tabPageMain.Name = "tabPageMain";
+            tabPageMain.Padding = new Padding(3);
+            tabPageMain.Size = new Size(492, 380);
+            tabPageMain.TabIndex = 0;
+            tabPageMain.Text = "Основа";
+            tabPageMain.UseVisualStyleBackColor = true;
+            // 
+            // buttonDecreaseGames
+            // 
+            buttonDecreaseGames.Location = new Point(281, 126);
+            buttonDecreaseGames.Name = "buttonDecreaseGames";
+            buttonDecreaseGames.Size = new Size(205, 39);
+            buttonDecreaseGames.TabIndex = 7;
+            buttonDecreaseGames.Text = "-1 счётчик игр";
+            buttonDecreaseGames.UseVisualStyleBackColor = true;
+            buttonDecreaseGames.Click += buttonDecreaseGames_Click;
+            // 
+            // tabPageCurrentGame
+            // 
+            tabPageCurrentGame.Controls.Add(buttonRejectGame);
+            tabPageCurrentGame.Controls.Add(buttonAcceptGame);
+            tabPageCurrentGame.Controls.Add(listBoxCurrentGame);
+            tabPageCurrentGame.Controls.Add(buttonAddToCurrentGame);
+            tabPageCurrentGame.Controls.Add(comboBoxSearchPlayers);
+            tabPageCurrentGame.Location = new Point(4, 24);
+            tabPageCurrentGame.Name = "tabPageCurrentGame";
+            tabPageCurrentGame.Padding = new Padding(3);
+            tabPageCurrentGame.Size = new Size(492, 380);
+            tabPageCurrentGame.TabIndex = 1;
+            tabPageCurrentGame.Text = "Текущая игра";
+            tabPageCurrentGame.UseVisualStyleBackColor = true;
+            // 
+            // buttonRejectGame
+            // 
+            buttonRejectGame.Location = new Point(6, 344);
+            buttonRejectGame.Name = "buttonRejectGame";
+            buttonRejectGame.Size = new Size(480, 23);
+            buttonRejectGame.TabIndex = 4;
+            buttonRejectGame.Text = "Отклонить игру";
+            buttonRejectGame.UseVisualStyleBackColor = true;
+            buttonRejectGame.Click += buttonRejectGame_Click;
+            // 
+            // buttonAcceptGame
+            // 
+            buttonAcceptGame.Location = new Point(6, 315);
+            buttonAcceptGame.Name = "buttonAcceptGame";
+            buttonAcceptGame.Size = new Size(480, 23);
+            buttonAcceptGame.TabIndex = 3;
+            buttonAcceptGame.Text = "Принять игру";
+            buttonAcceptGame.UseVisualStyleBackColor = true;
+            buttonAcceptGame.Click += buttonAcceptGame_Click;
+            // 
+            // listBoxCurrentGame
+            // 
+            listBoxCurrentGame.FormattingEnabled = true;
+            listBoxCurrentGame.ItemHeight = 15;
+            listBoxCurrentGame.Location = new Point(6, 35);
+            listBoxCurrentGame.Name = "listBoxCurrentGame";
+            listBoxCurrentGame.Size = new Size(480, 274);
+            listBoxCurrentGame.TabIndex = 2;
+            // 
+            // buttonAddToCurrentGame
+            // 
+            buttonAddToCurrentGame.Location = new Point(311, 7);
+            buttonAddToCurrentGame.Name = "buttonAddToCurrentGame";
+            buttonAddToCurrentGame.Size = new Size(175, 23);
+            buttonAddToCurrentGame.TabIndex = 1;
+            buttonAddToCurrentGame.Text = "Добавить игрока";
+            buttonAddToCurrentGame.UseVisualStyleBackColor = true;
+            buttonAddToCurrentGame.Click += buttonAddToCurrentGame_Click;
+            // 
+            // comboBoxSearchPlayers
+            // 
+            comboBoxSearchPlayers.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            comboBoxSearchPlayers.AutoCompleteSource = AutoCompleteSource.RecentlyUsedList;
+            comboBoxSearchPlayers.FormattingEnabled = true;
+            comboBoxSearchPlayers.Location = new Point(6, 7);
+            comboBoxSearchPlayers.Name = "comboBoxSearchPlayers";
+            comboBoxSearchPlayers.Size = new Size(299, 23);
+            comboBoxSearchPlayers.TabIndex = 0;
+            comboBoxSearchPlayers.DropDown += comboBoxSearchPlayers_DropDown;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(512, 420);
+            Controls.Add(tabControl1);
+            MaximumSize = new Size(528, 459);
+            MinimumSize = new Size(528, 459);
+            Name = "Form1";
+            Text = "VanyaCounter";
+            Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPageMain.ResumeLayout(false);
+            tabPageMain.PerformLayout();
+            tabPageCurrentGame.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBoxPlayers;
+        private Button buttonAddPlayer;
+        private TextBox textBoxNewPlayer;
+        private Button buttonReset;
+        private Button buttonIncreaseGame;
+        private Button buttonDeletePlayer;
+        private Button buttonResetGames;
+        private TabControl tabControl1;
+        private TabPage tabPageMain;
+        private TabPage tabPageCurrentGame;
+        private ComboBox comboBoxSearchPlayers;
+        private Button buttonRejectGame;
+        private Button buttonAcceptGame;
+        private ListBox listBoxCurrentGame;
+        private Button buttonAddToCurrentGame;
+        private Button buttonDecreaseGames;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace VanyaCounter
+{
+    public partial class Form1 : Form
+    {
+        private string dataFilePath = "players.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPlayerList();
+        }
+
+        private void LoadPlayerList()
+        {
+            listBoxPlayers.Items.Clear();
+
+            if (!File.Exists(dataFilePath))
+                File.Create(dataFilePath).Close();
+
+            var lines = File.ReadAllLines(dataFilePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split(';');
+                if (parts.Length == 2)
+                {
+                    string name = parts[0];
+                    if (int.TryParse(parts[1], out int games))
+                    {
+                        listBoxPlayers.Items.Add($"Игрок: {name} — Игр: {games}");
+                    }
+                }
+            }
+        }
+
+        private void buttonAddPlayer_Click(object sender, EventArgs e)
+        {
+            string name = textBoxNewPlayer.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Введите имя игрока.");
+                return;
+            }
+
+            var lines = File.ReadAllLines(dataFilePath).ToList();
+            if (lines.Any(line => line.Split(';')[0].Equals(name, StringComparison.OrdinalIgnoreCase)))
+            {
+                MessageBox.Show("Такой игрок уже существует.");
+                return;
+            }
+
+            lines.Add($"{name};0");
+            File.WriteAllLines(dataFilePath, lines);
+            LoadPlayerList();
+            textBoxNewPlayer.Clear();
+        }
+
+        private void buttonIncreaseGame_Click(object sender, EventArgs e)
+        {
+            if (listBoxPlayers.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите игрока из списка.");
+                return;
+            }
+
+            var selectedLine = listBoxPlayers.SelectedItem.ToString();
+            var name = selectedLine.Split('—')[0].Replace("Игрок:", "").Trim();
+
+            var lines = File.ReadAllLines(dataFilePath).ToList();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var parts = lines[i].Split(';');
+                if (parts[0] == name && int.TryParse(parts[1], out int games))
+                {
+                    games++;
+                    lines[i] = $"{name};{games}";
+                    break;
+                }
+            }
+
+            File.WriteAllLines(dataFilePath, lines);
+            LoadPlayerList();
+        }
+
+        private void buttonDeletePlayer_Click(object sender, EventArgs e)
+        {
+            if (listBoxPlayers.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите игрока для удаления.");
+                return;
+            }
+
+            var selectedLine = listBoxPlayers.SelectedItem.ToString();
+            var name = selectedLine.Split('—')[0].Replace("Игрок:", "").Trim();
+
+            var lines = File.ReadAllLines(dataFilePath).ToList();
+            lines = lines.Where(line => !line.StartsWith(name + ";")).ToList();
+
+            File.WriteAllLines(dataFilePath, lines);
+            LoadPlayerList();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(dataFilePath))
+                return;
+
+            var lines = File.ReadAllLines(dataFilePath)
+                .Where(line => !string.IsNullOrWhiteSpace(line) && line.Contains(';'))
+                .Select(line =>
+                {
+                    var parts = line.Split(';');
+                    string name = parts[0].Trim();
+                    int games = int.TryParse(parts[1], out int g) ? g : 0;
+                    return new { name, games };
+                })
+                .OrderByDescending(player => player.games)
+                .ToList();
+
+            File.WriteAllLines(dataFilePath, lines.Select(p => $"{p.name};{p.games}"));
+            LoadPlayerList();
+        }
+
+        private void buttonResetGames_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(dataFilePath))
+                return;
+
+            var lines = File.ReadAllLines(dataFilePath)
+                .Where(line => !string.IsNullOrWhiteSpace(line) && line.Contains(';'))
+                .Select(line =>
+                {
+                    var parts = line.Split(';');
+                    string name = parts[0].Trim();
+                    return $"{name};0";
+                })
+                .ToList();
+
+            File.WriteAllLines(dataFilePath, lines);
+            LoadPlayerList();
+        }
+
+        private void UpdatePlayerDropdown()
+        {
+            if (!File.Exists(dataFilePath)) return;
+
+            var names = File.ReadAllLines(dataFilePath)
+                .Where(l => l.Contains(";"))
+                .Select(l => l.Split(';')[0])
+                .Distinct()
+                .ToList();
+
+            comboBoxSearchPlayers.Items.Clear();
+            comboBoxSearchPlayers.Items.AddRange(names.ToArray());
+        }
+
+        private void buttonAddToCurrentGame_Click(object sender, EventArgs e)
+        {
+            string player = comboBoxSearchPlayers.Text.Trim();
+            if (string.IsNullOrEmpty(player)) return;
+
+            if (!listBoxCurrentGame.Items.Contains(player))
+                listBoxCurrentGame.Items.Add(player);
+        }
+
+        private void buttonAcceptGame_Click(object sender, EventArgs e)
+        {
+            if (listBoxCurrentGame.Items.Count == 0) return;
+
+            var lines = File.ReadAllLines(dataFilePath).ToList();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var parts = lines[i].Split(';');
+                if (parts.Length != 2) continue;
+
+                string name = parts[0];
+                if (listBoxCurrentGame.Items.Contains(name) && int.TryParse(parts[1], out int games))
+                {
+                    lines[i] = $"{name};{games + 1}";
+                }
+            }
+
+            File.WriteAllLines(dataFilePath, lines);
+            listBoxCurrentGame.Items.Clear();
+            LoadPlayerList();
+            UpdatePlayerDropdown();
+        }
+
+        private void buttonRejectGame_Click(object sender, EventArgs e)
+        {
+            listBoxCurrentGame.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadPlayerList();
+            UpdatePlayerDropdown();
+        }
+
+        private void comboBoxSearchPlayers_DropDown(object sender, EventArgs e)
+        {
+            string input = comboBoxSearchPlayers.Text.Trim().ToLower();
+
+            if (!File.Exists(dataFilePath)) return;
+
+            var players = File.ReadAllLines(dataFilePath)
+                .Where(l => l.Contains(";"))
+                .Select(l => l.Split(';')[0])
+                .Distinct()
+                .ToList();
+
+            // Фильтрация и сортировка по релевантности
+            var sorted = players
+                .OrderBy(p =>
+                {
+                    var lower = p.ToLower();
+                    if (lower.StartsWith(input)) return 0;
+                    if (lower.Contains(input)) return 1;
+                    return 2;
+                })
+                .ThenBy(p => p) // Алфавитный порядок внутри группы
+                .ToList();
+
+            comboBoxSearchPlayers.Items.Clear();
+            comboBoxSearchPlayers.Items.AddRange(sorted.ToArray());
+
+            // Для корректной работы SuggestAppend
+            comboBoxSearchPlayers.SelectionStart = comboBoxSearchPlayers.Text.Length;
+        }
+
+        private void buttonDecreaseGames_Click(object sender, EventArgs e)
+        {
+            if (listBoxPlayers.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите игрока из списка.");
+                return;
+            }
+
+            var selectedLine = listBoxPlayers.SelectedItem.ToString();
+            var name = selectedLine.Split('—')[0].Replace("Игрок:", "").Trim();
+
+            var lines = File.ReadAllLines(dataFilePath).ToList();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var parts = lines[i].Split(';');
+                if (parts[0] == name && int.TryParse(parts[1], out int games))
+                {
+                    if (games > 0)
+                        games--;
+                    lines[i] = $"{name};{games}";
+                    break;
+                }
+            }
+
+            File.WriteAllLines(dataFilePath, lines);
+            LoadPlayerList();
+        }
+    }
+}
